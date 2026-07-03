eafa7165c0bb6228bc8c8803e95b3e2ee917786d
Lars316 <144122@student.dcterra.nl>
2026-07-03 13:24:04 +0200

    Route gefixt
    De route specificeerde onnodig twee keer dezelfde informatie. Dat is nu opgelost.

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,10 @@ use Illuminate\Support\Carbon;
 
 Route::get('/', function () {
     try {
-        $fromDate = Carbon::parse('2026-07-02')->startOfDay()->utc();
-        $tillDate = $fromDate->copy()->addDay()->utc();
+        $date = Carbon::parse('2026-07-02')->format('d-m-Y');
 
-        $jsonPad = 'https://public.api.energyzero.nl/public/v1/prices?date=03-07-2026&interval=INTERVAL_HOUR&energyType=ENERGY_TYPE_ELECTRICITY';
-        $response = Http::get($jsonPad, [
-            'fromDate' => $fromDate->format('Y-m-d\TH:i:s\Z'),
-            'tillDate' => $tillDate->format('Y-m-d\TH:i:s\Z'),
-            'interval' => 'PT1H',
-            'usageType' => 1,
-            'inclBtw' => 'true',
-        ]);
+        $jsonPad = "https://public.api.energyzero.nl/public/v1/prices?date={$date}&interval=INTERVAL_HOUR&energyType=ENERGY_TYPE_ELECTRICITY";
+        $response = Http::get($jsonPad);
 
         $gegevens = $response->successful() ? $response->json() : [];
         $prijzen = $gegevens['Prices'] ?? [];
