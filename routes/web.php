655c1b665901b0d7eed58687bdfd0c0649346f3b
Lars316 <144122@student.dcterra.nl>
2026-07-03 10:26:19 +0200

    Data ophalen
    Ik haal de data op uit de json file via de route, en display die data daarna in een lijst op de welcome.blade pagina.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $jsonPad = base_path('energy_prices.json');
+    $gegevens = [];
+
+    if (file_exists($jsonPad)) {
+        $inhoud = file_get_contents($jsonPad);
+        $gegevens = json_decode($inhoud, true) ?? [];
+    }
+
+    $prijzen = $gegevens['Prices'] ?? [];
+
+    return view('welcome', compact('prijzen', 'gegevens'));
 });
